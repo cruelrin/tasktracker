2bf9fb34ddf5fd2503fa796d78dd5384f801a6fc
cruelrin <sheveleva_k@iuca.kg>
2024-03-06 02:58:43 +0600

    4

--- mytask/settings.py
+++ mytask/settings.py
@@ -122,8 +122,10 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "tasktracker", "static")]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'tasktracker', 'static')]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+DEBUG = True
